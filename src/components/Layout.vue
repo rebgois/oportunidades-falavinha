3f59ca5b13ab3144b31a5338aaa66ecad3f48c3c
Rebeca Gois <99913743+rebgois@users.noreply.github.com>
2025-06-17 02:31:18 -0300

    Tornar layout responsivo para mobile

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,14 +1,25 @@
 <template>
-    <v-app>
-      <Sidebar />
-      <v-main class="bg-BgPadrao">
-        <v-container fluid >
-          <slot />
-        </v-container>
-      </v-main>
-    </v-app>
-  </template>
-  
-  <script setup>
-  import Sidebar from '@/components/Sidebar.vue'
-  </script>
+  <v-app>
+    <Sidebar v-model="drawer" />
+    <v-app-bar class="d-md-none" color="PrimaryFalavinha" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <img
+        src="@/assets/Logotipo-FalavinhaNextWhite.png"
+        height="32"
+        class="ml-2"
+      />
+    </v-app-bar>
+    <v-main class="bg-BgPadrao">
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Sidebar from '@/components/Sidebar.vue'
+
+const drawer = ref(false)
+</script>
